a62ccc5946e595bd5506c66fd5f436b2607266e8
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2021-09-22 23:28:50 +0530

    test3

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 console.log('in container!');
+console.log('domain---', process.env.MARKETING_APP_DOMAIN);
 
 ReactDOM.render(<App />, document.querySelector('#root'));
