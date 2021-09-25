81dc24792edd9061d81295b878f3f44d56929128
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2021-09-25 19:52:07 +0530

    css conflict issue fix
    using
    const generateClassName = createGenerateClassName({
      productionPrefix: 'co',
    });

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -8,15 +8,15 @@ import {
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
-/** will uncomment this in next commit to resolve css conflict issue in production env */
-// const generateClassName = createGenerateClassName({
-//   productionPrefix: 'co',
-// });
+// Generating random className in production starting with 'co' to avoid conflicting with other applications
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
           <MarketingApp />
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// Generating random className in production starting with 'ma' to avoid conflicting with other applications
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
