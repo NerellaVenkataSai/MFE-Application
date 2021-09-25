2a5091469952d75c96209a8fdf9beb39b3923d7f
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2021-09-25 19:39:47 +0530

    css conflict issue reason
    using css in js is best option in MFE as it will generate random css classes.
    
    Note: In production that random classNames might get conflict with different apps. we will resolve that issue in next commit

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+/** will uncomment this in next commit to resolve css conflict issue in production env */
+// const generateClassName = createGenerateClassName({
+//   productionPrefix: 'co',
+// });
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
