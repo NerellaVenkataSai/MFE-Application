8620fe6effdc63227942c1b78ceee30ea40a0d6a
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2021-09-29 23:05:56 +0530

    test auth

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -5,19 +5,13 @@ import App from './App';
 
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-  /**  Default memory history path is '/' 
-   *   so if this application has specific path to load like 'auth/signup' 
-   *   then child application wont loads initially in container(host) as the child app has initial route as '/'
-   *   so it loads on second time when we try to change the path on click event.
-   *   To resolve that issue we will initialize memoryRoute with path
-   * 
-  */
-   const history = defaultHistory || createMemoryHistory({
-    initialEntries: [initialPath],
-  });
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
 
   if (onNavigate) {
-    console.log('onNavigate Auth 1', history.location.pathname);
     history.listen(onNavigate);
   }
 
@@ -36,7 +30,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 // If we are in development and in isolation,
 // call mount immediately
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
